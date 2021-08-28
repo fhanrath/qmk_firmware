3dc2a7e08ebcb7956f85425335bc6fd3c556a4bb
fenris <3193081+fhanrath@users.noreply.github.com>
2021-08-28 20:21:49 +0200

    fix mod keys

--- keyboards/kyria/keymaps/fenris_layout/keymap.c
+++ keyboards/kyria/keymaps/fenris_layout/keymap.c
@@ -86,8 +86,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAKDH] = LAYOUT(
           KC_ESC,    KC_Q,   KC_W,   KC_F,   KC_P,   KC_B,                                         KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_PIPE,
           KC_TAB,    KC_A,   KC_R,   KC_S,   KC_T,   KC_G,                                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT,
-        MOD_LCTL,    KC_Z,   KC_X,   KC_C,   KC_D,   KC_V,   KC_ENT,   KC_EQL,  KC_DEL, KC_BSPC,   KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-                          XXXXXXX, TO(_NAVIGATION), MOD_LALT, KC_SPC, KC_LCMD, MO(_UMLAUT), KC_LSFT, MO(_SYMBOL),  TO(_NUMBERS), XXXXXXX
+         KC_LCTL,    KC_Z,   KC_X,   KC_C,   KC_D,   KC_V,   KC_ENT,   KC_EQL,  KC_DEL, KC_BSPC,   KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
+                          XXXXXXX, TO(_NAVIGATION), KC_LOPT, KC_SPC, KC_LCMD, MO(_UMLAUT), KC_LSFT, MO(_SYMBOL),  TO(_NUMBERS), XXXXXXX
     ),
 /*
  * Number Layout 
