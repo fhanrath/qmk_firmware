7808f49656122fb0a7b171d0833099c1407c5b92
fenris <3193081+fhanrath@users.noreply.github.com>
2021-10-10 00:27:13 +0200

    my crkbd layout... not ready yet

---
+++ keyboards/crkbd/keymaps/fenris_layout/rules.mk
@@ -0,0 +1,9 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+LTO_ENABLE = yes
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+
+BOOTLOADER = atmel-dfu
