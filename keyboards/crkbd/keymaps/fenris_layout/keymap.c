366e20984e730af80865241aaa75fbacf1b14795
fenris <3193081+fhanrath@users.noreply.github.com>
2021-10-11 21:18:15 +0200

    fixed number layer

--- keyboards/crkbd/keymaps/fenris_layout/keymap.c
+++ keyboards/crkbd/keymaps/fenris_layout/keymap.c
@@ -65,7 +65,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
-
+ßß
 // Tap Dance
 
 qk_tap_dance_action_t tap_dance_actions[] = {
@@ -92,11 +92,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       COLEMAK, XXXXXXX,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                      XXXXXXX,    KC_7,    KC_8,    KC_9, KC_SCLN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_LGUI,   KC_F4,   KC_F5,   KC_F6,  KC_F11,                      XXXXXXX,    KC_4,    KC_5,    KC_6, XXXXXXX, XXXXXXX,
+      XXXXXXX, KC_LGUI,   KC_F4,   KC_F5,   KC_F6,  KC_F11,                      XXXXXXX,    KC_4,    KC_5,    KC_6, KC_COMM,  KC_DOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_LALT,   KC_F1,   KC_F2,   KC_F3,  KC_F12,                      XXXXXXX,    KC_1,    KC_2,    KC_3, XXXXXXX, KC_MINS,
+      _______, KC_LALT,   KC_F1,   KC_F2,   KC_F3,  KC_F12,                      XXXXXXX,    KC_1,    KC_2,    KC_3,  KC_EQL, KC_MINS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          XXXXXXX, _______, _______,    _______, _______, XXXXXXX
+                                          XXXXXXX, _______, _______,    _______, _______,    KC_0
                                       //`--------------------------'  `--------------------------'
   ),
 
